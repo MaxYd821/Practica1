b5477ac7eedb5304c7aaf3833bb0018fb46ec85e
MaxYd821 <maxramirez0914@gmail.com>
2025-04-03 11:01:17 -0500

    Segundo commit

--- app/src/main/java/com/example/prueba1/MainActivity.java
+++ app/src/main/java/com/example/prueba1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.prueba1;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button btnShowMessage = findViewById(R.id.btnShowMessage);
+        TextView tvMessage = findViewById(R.id.tvMessage);
+
+        btnShowMessage.setOnClickListener(v ->{
+            Log.i("MAIN_APP","Click en boton");
+            tvMessage.setText(Integer.toString(count));
+            count++;
         });
     }
 }
